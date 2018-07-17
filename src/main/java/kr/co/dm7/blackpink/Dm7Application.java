87ef1ae75639120fec191ea641e9ae544a695cc6
doubleseven <dlagustjs@gmail.com>
2018-07-17 19:19:43 +0900

    init project strucure

---
+++ src/main/java/kr/co/dm7/blackpink/Dm7Application.java
@@ -0,0 +1,12 @@
+package kr.co.dm7.blackpink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dm7Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Dm7Application.class, args);
+    }
+}
