f8cde825f83b3db448900545963a03fd9b994a2b
APETOY <APETOY@APETOY-LENOVO>
2018-07-18 11:09:30 +0900

    일단 Security는 당장 관련이 없으니 비활성화 하고 하는게 어떨까요?

--- src/main/java/kr/co/dm7/blackpink/Dm7Application.java
+++ src/main/java/kr/co/dm7/blackpink/Dm7Application.java
@@ -2,8 +2,9 @@ package kr.co.dm7.blackpink;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class })
 public class Dm7Application {
 
     public static void main(String[] args) {
